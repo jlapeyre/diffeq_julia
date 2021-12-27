a37e7d0fab232b8be5c31de191992e078b5c1fcd
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-12-27 18:45:00 -0500

    Include version

--- diffeq_julia/__init__.py
+++ diffeq_julia/__init__.py
@@ -1,1 +1,3 @@
 from ._julia_project import julia, compile_diffeq_julia, update_diffeq_julia
+
+from ._version import __version__
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
-setup(name='diffeq_julia',
-      version='0.0.1',
-      description='Solving Differential Equations in Python',
-      classifiers=[
+version = {}
+with open("./diffeq_julia/_version.py") as fp:
+    exec(fp.read(), version)
+
+setup(
+    name='diffeq_julia',
+    version=version['__version__'],
+    description='Solving Differential Equations in Python',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
@@ -11,12 +16,11 @@ setup(name='diffeq_julia',
         'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Physics'
-      ],
-      keywords='differential equations stochastic ordinary delay differential-algebraic dae ode sde dde',
-      author='John Lapeyre',
-      license='MIT',
-      packages=['diffeq_julia'],
-      install_requires=['julia>=0.2'],
-      include_package_data=True,
-      zip_safe=False)
-
+    ],
+    keywords='differential equations stochastic ordinary delay differential-algebraic dae ode sde dde',
+    author='John Lapeyre',
+    license='MIT',
+    packages=['diffeq_julia'],
+    install_requires=['julia>=0.2'],
+    include_package_data=True,
+    zip_safe=False)
